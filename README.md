80371d03b25f3558bd053b70f5dbf0e3081c8123
guelowrd <gaylord.warner@gmail.com>
2023-08-03 09:43:01 +0100

    add box and govtoken contracts

---
+++ README.md
@@ -0,0 +1,3 @@
+1. The smart contract is controlled by a DAO
+2. Every transaction sent by the DAO has to be voted on
+3. Although it's a bad model leading to plutocracy, the current implementation uses ERC20 tokens for voting (this is just an exercise)
---
+++ src/Box.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+    uint256 private s_value;
+
+    event ValueChanged(uint256 value);
+
+    constructor(address initialOwner) Ownable(initialOwner) {
+        s_value = 0;
+    }
+
+    function store(uint256 value) public onlyOwner {
+        s_value = value;
+        emit ValueChanged(value);
+    }
+
+    function retrieve() public view returns (uint256) {
+        return s_value;
+    }
+}
