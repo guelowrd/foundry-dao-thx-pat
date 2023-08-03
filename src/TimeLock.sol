8d37a4774be2a942e8e88a977dbcd643594eee50
guelowrd <gaylord.warner@gmail.com>
2023-08-03 09:57:09 +0100

    add timelock

---
+++ src/TimeLock.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+
+    // mintDelay is how long one has to wait before eeuting
+    // proposers is the list of addresses that can propose
+    // executors is the list of addresses that can execute
+    constructor (uint256 mintDelay, address[] memory proposers, address[] memory executors) TimelockController(mintDelay, proposers, executors) {
+        TimelockController(mintDelay, proposers, executors msg.sender);
+    }
+}
